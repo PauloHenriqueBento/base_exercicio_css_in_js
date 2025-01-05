943709af398d1c41cd3458cf4793ab512d586276
PauloHenriqueBentoDosSantos <phbento@outlook.com.br>
2025-01-05 13:57:44 -0300

    Refactor styles to use styled-components and implement a primary theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
 import './global.css'
+import GlobalStyle, { Container } from './styles'
+import primaryTheme from './themes/primary'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={primaryTheme}>
+      <GlobalStyle />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
